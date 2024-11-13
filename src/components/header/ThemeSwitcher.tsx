14c0a9380578b182f2b0c21039a31a23eb3ab6ce
hyenee99 <twoddal2@naver.com>
2024-11-13 16:43:24 +0900

    Title 컴포넌트 생성

--- src/components/header/ThemeSwitcher.tsx
+++ src/components/header/ThemeSwitcher.tsx
@@ -1,9 +1,7 @@
 import { useContext } from "react";
-import { ThemeName } from "../../style/theme";
 import { ThemeContext } from "../../context/themeContext";
 
 function ThemeSwitcher(){
-
   const {themeName, toggleTheme} = useContext(ThemeContext);
 
   return (
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,9 +1,16 @@
 export type ThemeName = "light" | "dark";
-type ColorKey = "primary" | "background" | "secondary" | "third" ;
+export type ColorKey = "primary" | "background" | "secondary" | "third" ;
+
+export type HeadingSize = "large"| "medium"| "small";
 
 interface Theme {
   name: ThemeName;
   color: Record<ColorKey , string>;
+  heading: {
+    [key in HeadingSize]: {
+      fontSize: string;
+    }
+  }
 }
 
 export const light:Theme = {
@@ -13,10 +20,22 @@ export const light:Theme = {
     background: "lightgray",
     secondary: "blue",
     third: "green"
+  },
+  heading : {
+    large:{
+      fontSize:"2rem"
+    },
+    medium: {
+      fontSize:"1.5rem"
+    },
+    small: {
+      fontSize:"1rem"
+    }
   }
 };
 
 export const dark:Theme= {
+  ...light,
   name: 'dark',
   color: {
     primary: 'coral',
