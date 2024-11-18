1ab10e6b9df4cbef80ac97f4e84e2c41c05352ba
hyenee99 <twoddal2@naver.com>
2024-11-18 22:11:44 +0900

    도서 상세설명 접기 펼치기 구현

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -6,6 +6,7 @@ import Title from "../components/common/Title";
 import { BookDetail as IBookDetail } from "../models/book.model";
 import { formatDate, formatNumber } from "../utils/format";
 import { Link } from "react-router-dom";
+import EllipsisBox from "../components/common/EllipsisBox";
 
 const bookInfoList = [
   {
@@ -76,7 +77,15 @@ function BookDetail() {
           <div className="add-cart">장바구니 넣기</div>
         </div>
       </header>
-      <div className="content"></div>
+      <div className="content">
+        <Title size="medium">상세 설명</Title>
+        <EllipsisBox linelimit={4}>
+          {book.detail}
+        </EllipsisBox>
+
+        <Title size="medium">목차</Title>
+        <p className="index">{book.contents}</p>
+      </div>
     </BookDetailStyle>
   );
 }
@@ -113,6 +122,9 @@ const BookDetailStyle = styled.div`
           color: ${({ theme }) => theme.color.primary};
         }
       }
+    }
+
+    .content {
     }
   }
 `;
