e016f01702cbeb66f1f2094db39a7240f9e83139
hyenee99 <twoddal2@naver.com>
2024-11-18 23:50:28 +0900

    도서 상세페이지 장바구니 넣기 구현

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -3,9 +3,11 @@ import { BookDetail } from "../models/book.model";
 import { fetchBook, likeBook, unlikeBook } from "../api/books.api";
 import { useAuthStore } from "../store/authStore";
 import { useAlert } from "./useAlert";
+import { addCart } from "../api/carts.api";
 
 export const useBook = (bookId: string | undefined) => {
   const [book, setBook] =useState<BookDetail | null>(null);
+  const [cartAdded, setCartAdded] = useState(false);
   const {isloggedIn} = useAuthStore();
   const showAlert = useAlert();
 
@@ -40,6 +42,20 @@ export const useBook = (bookId: string | undefined) => {
     }
   };
 
+  const addToCart = (quantity: number) => {
+    if (!book) return;
+    
+    addCart({
+      book_id: book.id,
+      quantity: quantity,
+    }).then(() => {
+      setCartAdded(true);
+      setTimeout(() => {
+         setCartAdded(false); 
+      },3000);
+    });
+  };
+
   useEffect(() => {
     if(!bookId) return;
     
@@ -48,5 +64,5 @@ export const useBook = (bookId: string | undefined) => {
     });
   },[bookId]);
 
-  return {book, likeToggle};
+  return {book, likeToggle, addToCart, cartAdded};
 }
--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -8,6 +8,7 @@ import { formatDate, formatNumber } from "../utils/format";
 import { Link } from "react-router-dom";
 import EllipsisBox from "../components/common/EllipsisBox";
 import LikeButton from "../components/book/LikeButton";
+import AddToCart from "../components/book/AddToCart";
 
 const bookInfoList = [
   {
@@ -77,7 +78,9 @@ function BookDetail() {
           <div className="like">
             <LikeButton book={book} onClick={likeToggle} />
           </div>
-          <div className="add-cart">장바구니 넣기</div>
+          <div className="add-cart">
+            <AddToCart book={book}/>
+          </div>
         </div>
       </header>
       <div className="content">
