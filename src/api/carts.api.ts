79145fd06c3e7f0ac6ff76155434a9f38ebec7e0
hyenee99 <twoddal2@naver.com>
2024-11-19 23:46:21 +0900

    장바구니 목록 구현

--- src/api/carts.api.ts
+++ src/api/carts.api.ts
@@ -1,3 +1,4 @@
+import { Cart } from "../models/cart.model";
 import { httpClient } from "./http";
 
 interface AddCartParams {
@@ -8,4 +9,14 @@ interface AddCartParams {
 export const addCart = async(params :AddCartParams ) => {
   const response = await httpClient.post("/carts", params);
   return response.data;
+}
+
+export const fetchCart = async() => {
+  const response = await httpClient.get<Cart[]>("/carts");
+  return response.data;
+}
+
+export const deleteCart = async(cartId: number) => {
+ const response = await httpClient.delete(`/carts/${cartId}`);
+ return response.data; 
 }
---
+++ src/components/common/Empty.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Title from "../common/Title";
+
+interface Props {
+  icon?: React.ReactNode;
+  title: string;
+  description?: React.ReactNode;
+}
+function Empty({icon, title, description}: Props) {
+  return (
+    <EmptyStyle>
+      {icon &&  <div className="icon">{icon}</div>}
+      <Title size="large" color="secondary">
+        {title}
+      </Title>
+      {description && <p>{description}</p>}
+    </EmptyStyle>
+  );
+}
+
+const EmptyStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  padding: 120px 0;
+
+  .icon {
+    svg {
+      font-size: 4rem;
+      fill: #ccc;
+    }
+  }
+`;
+
+export default Empty;
