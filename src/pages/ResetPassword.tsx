61aac0acc13f84550f4dd02b27ebb41aa4d68936
hyenee99 <twoddal2@naver.com>
2024-11-22 15:39:26 +0900

    useAuth 훅 생성

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -3,10 +3,8 @@ import InputText from "../components/common/InputText";
 import Button from "../components/common/Button";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { resetPassword, resetRequest, signup } from "../api/auth.api";
-import { useAlert } from "../hooks/useAlert";
 import { SignupStyle } from "./Signup";
-import { useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
 
 export interface SignupProps {
   email: string;
@@ -14,9 +12,7 @@ export interface SignupProps {
 }
 
 function ResetPassword() {
-  const navigate = useNavigate();
-  const {showAlert} = useAlert();
-  const [resetRequested, setResetRequested] = useState(false);
+  const {userResetPassword, userResetRequest, resetRequested} = useAuth();
 
   const {
     register,
@@ -25,19 +21,7 @@ function ResetPassword() {
   } = useForm<SignupProps>();
 
   const onSubmit = (data: SignupProps) => {
-    if(resetRequested) {
-      // 초기화
-      resetPassword(data).then(()=>{
-        showAlert("비밀번호가 초기화되었습니다.");
-        navigate('./login');
-      });
-    }
-    else {
-      // 요청
-      resetRequest(data).then(()=> {
-        setResetRequested(true);
-      });
-    }
+    resetRequested  ? userResetPassword(data) : userResetRequest(data);
   };
 
   return (
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -4,8 +4,7 @@ import InputText from '../components/common/InputText';
 import Button from '../components/common/Button';
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { signup } from '../api/auth.api';
-import { useAlert } from '../hooks/useAlert';
+import { useAuth } from '@/hooks/useAuth';
 
 export interface SignupProps {
   email: string;
@@ -13,16 +12,12 @@ export interface SignupProps {
 }
 
 function SignUp() {
-  const navigate = useNavigate();
-  const {showAlert} = useAlert();
+  const {userSignup}= useAuth();
   const {register, handleSubmit, formState: {errors}} = useForm<SignupProps>();
 
   const onSubmit = (data: SignupProps) => {
-    signup(data).then((res) => {
-      showAlert('회원가입이 완료되었습니다.');
-      navigate("/login");
-    })
-  }
+    userSignup(data);
+  };
 
   return (
     <>
