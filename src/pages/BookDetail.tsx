bc010d9408eb88e312e58b1115067ea39a805159
hyenee99 <twoddal2@naver.com>
2024-11-23 18:01:53 +0900

    리뷰 작성 구현

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -51,7 +51,7 @@ const bookInfoList = [
 
 function BookDetail() {
   const { bookId } = useParams();
-  const { book, likeToggle, reviews} = useBook(bookId);
+  const { book, likeToggle, reviews, addReview} = useBook(bookId);
 
   if (!book) return null;
 
@@ -94,7 +94,7 @@ function BookDetail() {
         <p className="index">{book.contents}</p>
 
         <Title size="medium">리뷰</Title>
-        <BookReview reviews={reviews}/>
+        <BookReview reviews={reviews} onAdd={addReview}/>
       </div>
     </BookDetailStyle>
   );
