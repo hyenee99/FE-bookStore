c830cdb5318536ce90196096d7a0a04eae9bbd50
hyenee99 <twoddal2@naver.com>
2024-11-25 16:40:51 +0900

    메인 화면- 베스트 섹션 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,19 @@
 import Title from "@/components/common/Title";
+import MainBest from "@/components/Main/MainBest";
 import MainNewBooks from "@/components/Main/MainNewBooks";
 import MainReview from "@/components/Main/MainReview";
 import { useMain } from "@/hooks/useMain";
 import styled from "styled-components";
 
 function Home() {
-  const {reviews, newBooks} = useMain();
+  const {reviews, newBooks, bestBooks} = useMain();
   return (
     <HomeStyle>
       {/* 배너 */}
       {/* 베스트셀러 */}
       <section className="section">
         <Title size="large">베스트 셀러</Title>
+        <MainBest books={bestBooks} />
       </section>
       {/* 신간 */}
       <section className="section">
@@ -27,5 +29,8 @@ function Home() {
   );
 }
 const HomeStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
 export default Home;
