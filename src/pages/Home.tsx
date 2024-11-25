383a638f1520923f1a260c60e03ab00c8ee47f38
hyenee99 <twoddal2@naver.com>
2024-11-25 16:10:46 +0900

    메인 화면- 신간 섹션 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,31 @@
+import Title from "@/components/common/Title";
+import MainNewBooks from "@/components/Main/MainNewBooks";
 import MainReview from "@/components/Main/MainReview";
 import { useMain } from "@/hooks/useMain";
 import styled from "styled-components";
 
 function Home() {
-  const {reviews} = useMain();
+  const {reviews, newBooks} = useMain();
   return (
     <HomeStyle>
       {/* 배너 */}
       {/* 베스트셀러 */}
+      <section className="section">
+        <Title size="large">베스트 셀러</Title>
+      </section>
       {/* 신간 */}
+      <section className="section">
+        <Title size="large">신간 안내</Title>
+        <MainNewBooks books={newBooks} />
+      </section>
       {/* 리뷰 */}
-      <MainReview reviews={reviews}/>
+      <section className="section">
+        <Title size="large">리뷰</Title>
+        <MainReview reviews={reviews}/>
+      </section>
     </HomeStyle>
   );
 }
-const HomeStyle = styled.div``;
+const HomeStyle = styled.div`
+`;
 export default Home;
