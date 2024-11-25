f479ef2f121d17acdd01f8d79466294deab1f14e
hyenee99 <twoddal2@naver.com>
2024-11-25 15:57:40 +0900

    메인 화면- 리뷰 섹션 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
+import MainReview from "@/components/Main/MainReview";
+import { useMain } from "@/hooks/useMain";
 import styled from "styled-components";
 
 function Home() {
+  const {reviews} = useMain();
   return (
     <HomeStyle>
-      <h1>Home</h1>
+      {/* 배너 */}
+      {/* 베스트셀러 */}
+      {/* 신간 */}
+      {/* 리뷰 */}
+      <MainReview reviews={reviews}/>
     </HomeStyle>
   );
 }
